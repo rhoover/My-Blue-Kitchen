7b62d35eae9c94e789ed78414fac4a89e2f534c8
rhoover <robin.hoover@gmail.com>
2023-06-29 17:29:26 -0400

    Events Page Creation

---
+++ template-parts/content-events.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Template part for displaying posts in page-events.php
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package mbk
+ */
+?>
+
+<h2 class="events-list-header">My 2023 Appearances</h2>
+<section class="events-items">
+  <div class="events-item">
+    <h3 class="events-item-header">Montpelier Farmers Market</h3>
+    <p class="events-item-daytime">Saturdays 9:00am - 1:00pm</p>
+    <p class="events-item-days">July: 8th, 15th, 22nd, 29th</p>
+    <p class="events-item-days">August: 5th, 12th, 19th, 26th</p>
+    <p class="events-item-days">September: 2nd, 9th, 16th, 23rd, 30th</p>
+    <p class="events-item-days">October: 7th, 14th, 21st, 28th</p>
+  </div>
+  <div class="events-item">
+    <h3 class="events-item-header">Waterbury Farmers Market</h3>
+    <p class="events-item-daytime">Thursdays 4:00pm - 7:00pm</p>
+    <p class="events-item-days">August: 31st</p>
+    <p class="events-item-days">September: 7th</p>
+  </div>
+  <div class="events-item">
+    <h3 class="events-item-header">Art On The Park - Stowe</h3>
+    <p class="events-item-daytime">Thursdays 5:00pm - 8:00pm</p>
+    <p class="events-item-days">July: 6th, 13th, 20th, 27th</p>
+    <p class="events-item-days">August: 3rd, 10th, 17th, 24th</p>
+  </div>
+  <div class="events-item">
+    <h3 class="events-item-header">Taste of New England - Stowe</h3>
+    <p class="events-item-daytime">Sunday, 12:00pm - 4:00pm</p>
+    <p class="events-item-days">August: 27th</p>
+  </div>
+  <div class="events-item">
+    <h3 class="events-item-header">Autumn On The Green - Danville</h3>
+    <p class="events-item-daytime">Sunday, 10:00am - 4:00pm</p>
+    <p class="events-item-days">October: 1st</p>
+  </div>
+</section>
+<div id="events-map" class="events-map"></div>
