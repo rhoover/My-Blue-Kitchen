60d13f273e49627f06d63243e4a2a2f2d024054f
rhoover <robin.hoover@gmail.com>
2023-10-06 14:28:33 -0400

    Updating Events

--- template-parts/content-events.php
+++ template-parts/content-events.php
@@ -19,7 +19,7 @@
     <p class="events-item-days">February: 10th, 24th</p>
     <p class="events-item-days">March: 9th, 23rd</p>
     <p class="events-item-days">April: 13th, 27th</p>
-    <button class="events-item-button" data-id="montpelier-summer">
+    <button class="events-item-button" data-id="montpelier">
       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 54.757 54.757" height="25" width="25" role="img" title="map icon"><path d="M27.557 12c-3.86 0-7 3.14-7 7s3.14 7 7 7 7-3.14 7-7-3.14-7-7-7zm0 12c-2.757 0-5-2.243-5-5s2.243-5 5-5 5 2.243 5 5-2.243 5-5 5z"/><path d="M40.94 5.617C37.318 1.995 32.502 0 27.38 0c-5.123 0-9.938 1.995-13.56 5.617C7.117 12.32 6.284 24.93 12.016 32.57L27.38 54.756 42.72 32.6c5.756-7.67 4.923-20.28-1.78-26.983zm.16 25.814L27.38 51.244 13.64 31.4c-5.2-6.932-4.455-18.32 1.595-24.37C18.48 3.788 22.792 2 27.38 2s8.9 1.787 12.146 5.03c6.05 6.05 6.795 17.438 1.573 24.4z"/></svg>
       Find On The Map
     </button>
@@ -29,7 +29,7 @@
     <p class="events-item-daytime">10:00pm - 2:00pm</p>
     <p class="events-item-daytime">Held at Montpelier High School</p>
     <p class="events-item-days">November: 18th</p>
-    <button class="events-item-button" data-id="waterbury">
+    <button class="events-item-button" data-id="montpelier">
       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 54.757 54.757" height="25" width="25" role="img" title="map icon"><path d="M27.557 12c-3.86 0-7 3.14-7 7s3.14 7 7 7 7-3.14 7-7-3.14-7-7-7zm0 12c-2.757 0-5-2.243-5-5s2.243-5 5-5 5 2.243 5 5-2.243 5-5 5z"/><path d="M40.94 5.617C37.318 1.995 32.502 0 27.38 0c-5.123 0-9.938 1.995-13.56 5.617C7.117 12.32 6.284 24.93 12.016 32.57L27.38 54.756 42.72 32.6c5.756-7.67 4.923-20.28-1.78-26.983zm.16 25.814L27.38 51.244 13.64 31.4c-5.2-6.932-4.455-18.32 1.595-24.37C18.48 3.788 22.792 2 27.38 2s8.9 1.787 12.146 5.03c6.05 6.05 6.795 17.438 1.573 24.4z"/></svg>
       Find On The Map
     </button>
@@ -54,15 +54,15 @@
       Find On The Map
     </button>
   </div> -->
-  <!-- <div class="events-item">
-    <h3 class="events-item-header">Taste of New England - Stowe</h3>
-    <p class="events-item-daytime">Sunday, 12:00pm - 4:00pm</p>
-    <p class="events-item-days">August: 27th</p>
-    <button class="events-item-button" data-id="taste">
+  <div class="events-item">
+    <h3 class="events-item-header">Craftsbury Holiday Market</h3>
+    <p class="events-item-daytime">The exact date is still to be determined, 12:00pm - 4:00pm</p>
+    <!-- <p class="events-item-days">August: 27th</p> -->
+    <button class="events-item-button" data-id="craftsbury">
       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 54.757 54.757" height="25" width="25" role="img" title="map icon"><path d="M27.557 12c-3.86 0-7 3.14-7 7s3.14 7 7 7 7-3.14 7-7-3.14-7-7-7zm0 12c-2.757 0-5-2.243-5-5s2.243-5 5-5 5 2.243 5 5-2.243 5-5 5z"/><path d="M40.94 5.617C37.318 1.995 32.502 0 27.38 0c-5.123 0-9.938 1.995-13.56 5.617C7.117 12.32 6.284 24.93 12.016 32.57L27.38 54.756 42.72 32.6c5.756-7.67 4.923-20.28-1.78-26.983zm.16 25.814L27.38 51.244 13.64 31.4c-5.2-6.932-4.455-18.32 1.595-24.37C18.48 3.788 22.792 2 27.38 2s8.9 1.787 12.146 5.03c6.05 6.05 6.795 17.438 1.573 24.4z"/></svg>
       Find On The Map
     </button>
-  </div> -->
+  </div>
   <!-- <div class="events-item">
     <h3 class="events-item-header">Autumn On The Green - Danville</h3>
     <p class="events-item-daytime">Sunday, 10:00am - 4:00pm</p>
