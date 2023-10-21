7c7d3f921d72d5b54de0d3c8fe54ac91ab37174d
rhoover <robin.hoover@gmail.com>
2023-10-21 14:20:25 -0400

    Improved Share Button

--- js/share-everywhere.js
+++ js/share-everywhere.js
@@ -5,8 +5,18 @@
   const pageURL = document.querySelector("link[rel=canonical]");
   const pageTitle = document.title;
 
+  const contentElem = document.querySelector('.content');
+  const contentElemPos = contentElem.offsetTop;
+
   if (navigator.share) {
-    homeShareButton.style.display = 'block';
+    window.addEventListener('scroll', function () {
+      let scrollPos = window.scrollY;
+      if (scrollPos >= contentElemPos) {
+        homeShareButton.style.display = 'block';
+      } else {
+        homeShareButton.style.display = 'none';  
+      };
+    });
     homeShareButton.addEventListener('click', () =>
       navigator.share({
         title: pageTitle,
