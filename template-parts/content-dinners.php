38307fb783a022786c9028b730b1bd00bd88aab5
rhoover <robin.hoover@gmail.com>
2024-08-17 15:37:52 -0400

    New Dinners Page

---
+++ template-parts/content-dinners.php
@@ -0,0 +1,180 @@
+<?php
+/**
+ * Template part for displaying posts in page-dinners.php
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package mbk
+ */
+?>
+
+<h2 class="dinners-header">It has been such a treat to present so much lovely and delicious food at our semi-regular pop-up dinners right here in Stowe!</h2>
+
+<p class="dinners-text-intro">For your reading (and drooling) pleasure, <strong>here are the menus we've done so far.</strong> Stick around, there's alot more to come.</p>
+
+<section class="dinners-buttons">
+  <a href="#spain" class="dinners-button-link">
+    <button class="dinners-button">Flavors of Spain</button>
+  </a>
+  <a href="#neworleans" class="dinners-button-link">
+    <button class="dinners-button">Flavors of New Orleans</button>
+  </a>
+  <a href="#southamerica" class="dinners-button-link">
+    <button class="dinners-button">Flavors of South America</button>
+  </a>
+  <a href="#northafrica" class="dinners-button-link">
+    <button class="dinners-button">Flavors of North Africa</button>
+  </a>
+  <a href="#southeastasia" class="dinners-button-link">
+    <button class="dinners-button">Flavors of Southeast Asia</button>
+  </a>
+</section>
+
+<section class="dinners-menu" id="spain">
+  <h2 class="dinners-header">Flavors of Spain</h2>
+  <h2 class="dinners-menu-header">APPS</h2>
+
+  <hr class="dinners-menu-line"/>
+
+  <p class="dinners-menu-item">Merguez Sausage | Leek Jam | Fino Sherry Reduction</p>
+  <p class="dinners-menu-item">Marinated Trout | Crispy Zucchini | Saffron-Egg Emulsion</p>
+  <p class="dinners-menu-item">Eggplant Fries | Garlic-Paprika Aioli</p>
+
+  <h2 class="dinners-menu-header">ENTREES</h2>
+
+  <hr class="dinners-menu-line"/>
+
+  <p class="dinners-menu-item">Seared Pork Loin | Rioja Style Vegetable Medley | Sweet Pepper Sauce</p>
+  <p class="dinners-menu-item">Halibut Filet | Artichoke-Saffron Vegetables | Green Garlic Sauce</p>
+  <p class="dinners-menu-item">Paella Rice | Manchego | Garlic Green Beans</p>
+
+  <h2 class="dinners-menu-header">DESSERTS</h2>
+
+  <hr class="dinners-menu-line"/>
+
+  <span id="neworleans"></span>
+  <p class="dinners-menu-item">Orange Rice Pudding | Orange Champagne</p>
+  <p class="dinners-menu-item">Almond-Honey Ice Cream</p>
+  <p class="dinners-menu-item">Pears In Wine | Lemon Snap Cookie</p>
+</section>
+<section class="dinners-menu">
+
+  <h2 class="dinners-header">Flavors of New Orleans</h2>
+
+  <h2 class="dinners-menu-header">APPS</h2>
+
+  <hr class="dinners-menu-line"/>
+
+  <p class="dinners-menu-item">Louisiana Roll: Shrimp | Rice | Cajun Veg Mix | Sweet Pepper Sauce</p>
+  <p class="dinners-menu-item">Gumbo: Chicken | Lambdouille Sausage | Okra</p>
+  <p class="dinners-menu-item">Po'Boy Fried Oyster Salad: Greens - Sweet and Bitter | Creamy Mignonette Dressing</p>
+
+  <h2 class="dinners-menu-header">ENTREES</h2>
+
+  <hr class="dinners-menu-line"/>
+
+  <p class="dinners-menu-item">Seafood Court Boullion | Haddock | Cod | Rich Tomato Broth</p>
+  <p class="dinners-menu-item">Kebabs: Shrimp | Sausage | Black-Eyed Peas | White Wine Reduction</p>
+  <p class="dinners-menu-item">Vegetable Jambalaya Cakes | Wilted Greens | Red Gravy</p>
+  <p class="dinners-menu-item">Bourbon Glazed Chicken | Stewed White Beans and Vegetables</p>
+
+  <h2 class="dinners-menu-header">DESSERTS</h2>
+
+  <hr class="dinners-menu-line"/>
+
+  <span id="southamerica"></span>
+  <p class="dinners-menu-item">Mississippi Mud Pie</p>
+  <p class="dinners-menu-item">Butter Pecan Ice Cream</p>
+  <p class="dinners-menu-item">Classic Beignets</p>
+</section>
+<section class="dinners-menu">
+
+  <h2 class="dinners-header">Flavors of South America</h2>
+
+  <h2 class="dinners-menu-header">APPS</h2>
+
+  <hr class="dinners-menu-line"/>
+
+  <p  class="dinners-menu-item">Tuna Ceviche: Citrus | Crispy Sweet Potato Batons | Pickled Vegetables</p>
+  <p  class="dinners-menu-item">Empanadas: Black Bean Stuffing | Tostones | Panca Pepper Adobo</p>
+  <p  class="dinners-menu-item">Seared Provolone Cheese: Chimichurri Sauce</p>
+
+  <h2 class="dinners-menu-header">ENTREES</h2>
+
+  <hr class="dinners-menu-line"/>
+
+  <p  class="dinners-menu-item">Pulled Flank Steak Sandwich on Arepas Bun: Yuca Fries | Cilantro Aioli a la Presilla</p>
+  <p  class="dinners-menu-item">Bacon Wrapped Stuffed Pork Loin: Adobo | Picante Tamal</p>
+  <p  class="dinners-menu-item">Bahia Fish Stew: Coconut | Rice | Shrimp Stuffing</p>
+  <p  class="dinners-menu-item">Quinoa and Shiitake Mushroom Stew: Sofrito Vegetables | Chipotle Broth</p>
+
+  <h2 class="dinners-menu-header">DESSERTS</h2>
+
+  <hr class="dinners-menu-line"/>
+
+  <span id="northafrica"></span>
+  <p  class="dinners-menu-item">Cacao Truffle Assortment</p>
+  <p  class="dinners-menu-item">Orange Flan with Rhum Sauce</p>
+  <p  class="dinners-menu-item">Shortbread with Dulce de Leche</p>
+
+</section>
+<section class="dinners-menu" id="northafrica">
+
+  <h2 class="dinners-header">Flavors of North Africa</h2>
+  <h2 class="dinners-menu-header">APPS</h2>
+
+  <hr class="dinners-menu-line"/>
+
+  <p  class="dinners-menu-item">Clams Matala: Squash | Ginger | Roasted Peanuts | Chili</p>
+  <p  class="dinners-menu-item">Zucchini Fennel Salad | Preserved Lemon | Olives | Harissa Vinaigrette</p>
+  <p  class="dinners-menu-item">Cumin Shrimp | Carrot Orange Salad</p>
+
+  <h2 class="dinners-menu-header">ENTREES</h2>
+
+  <hr class="dinners-menu-line"/>
+
+  <p  class="dinners-menu-item">Chicken Fufu: Cassava | Sweet Potatoes | Spiced Onion Coulis</p>
+  <p  class="dinners-menu-item">Coffee-BBQ Glazed Pork Tenderloin | Jicama Slaw</p>
+  <p  class="dinners-menu-item">Vegetable Tagine: Yukon Potatoes | Parsnips | Eggplant | Gold Beets | Yoghurt</p>
+  <p  class="dinners-menu-item">Lemon-Seared Filet of Sole | Crispy Eggplant | Red Lentil Salad | Chermoula</p>
+
+  <h2 class="dinners-menu-header">DESSERTS</h2>
+
+  <hr class="dinners-menu-line"/>
+
+  <span id="southeastasia"></span>
+  <p  class="dinners-menu-item">Banana and Ginger Salad with Honey Ice Cream</p>
+  <p  class="dinners-menu-item">Malva Pudding with Apricot Coulis</p>
+  <p  class="dinners-menu-item">Coconut Qumbe with Mocha Sauce</p>
+
+</section>
+<section class="dinners-menu">
+
+  <h2 class="dinners-header">Flavors of Southeast Asia</h2>
+
+  <h2 class="dinners-menu-header">APPS</h2>
+
+  <hr class="dinners-menu-line"/>
+
+  <p  class="dinners-menu-item">Duck Bun Cha (Vietnam):  Duck Meatballs | Chili-Lime Dipping Sauce</p>
+  <p  class="dinners-menu-item">Khmer Chrouak (Cambodia):  Marinated Cucumber | Carrots | Green Beans | Cabbage</p>
+  <p  class="dinners-menu-item">Nam Kao (Laos): Crispy Rice Salad | Basil | Shallots | Spring Onions</p>
+
+  <h2 class="dinners-menu-header">ENTREES</h2>
+
+  <hr class="dinners-menu-line"/>
+
+  <p  class="dinners-menu-item">Beef Rendang (Indonesia):  Lemongrass | Coconut | Spices | Fried Noodles | Gold Potatoes</p>
+  <p  class="dinners-menu-item">Tom Kha Gai (Thailand): Coconut Chicken Soup | Curry | Mushrooms | Scallions</p>
+  <p  class="dinners-menu-item">Penang Hokkein Mee (Malaysia):  Prawn and Rice Noodle Soup</p>
+  <p  class="dinners-menu-item">Sayur Lodeh (Indonesia):  Jasmine Rice Cakes | Vegetables | Tempeh | Coconut Stew</p>
+
+  <h2 class="dinners-menu-header">DESSERTS</h2>
+
+  <hr class="dinners-menu-line"/>
+
+  <p  class="dinners-menu-item">Coconut Ice Cream | Fresh Roasted Peanuts (Vietnam)</p>
+  <p  class="dinners-menu-item">Mango Sorbet (Vietnam)</p>
+  <p  class="dinners-menu-item">Steamed Pumpkin Cake | Toasted Coconut | Fresh Yoghurt (Thailand)</p>
+
+</section>
