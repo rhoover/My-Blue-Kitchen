552d2c787742e3e64423729e83a129edd1e67119
rhoover <robin.hoover@gmail.com>
2024-09-07 12:10:09 -0400

    Fixed Close Button On Nav Menu

--- js/navclose.js
+++ js/navclose.js
@@ -1,30 +1,26 @@
 (() => {
   'use strict';
 
+  const allMenuItems = document.querySelectorAll('.menu-item');
   const navMenu = document.querySelector('.nav-main');
-  const closeItem = document.createElement('p');
-  const menuButton = document.querySelector('.header-menu');
   const menuText = document.querySelector('.menu-button-text');
   const menuButtonWrap = document.querySelector('.menu-toggle');
+  
+  allMenuItems.forEach((menuItem) => {
+    if (menuItem.innerText == "Close") {
+      menuItem.classList.add('nav-main-close');
+    }
+  });
+  const closeButton = document.querySelector('.nav-main-close');
 
-  closeItem.setAttribute('class', 'nav-main-close');
-  closeItem.textContent = 'Close';
-
-
-  // if (navigator.share) {
-  if (navigator.platform != "Windows" || navigator.platform != "MacIntel" || navigator.platform != "Linux x86_64") {
-    
-    navMenu.prepend(closeItem);
-
-    closeItem.onclick = () => {
-      navMenu.classList.toggle('nav-main-open');
-      menuButtonWrap.classList.toggle('opened');
-      menuText.classList.toggle('menu-button-text-red');
-      if (menuText.innerHTML === 'Menu') {
-        menuText.innerHTML = 'Close'
-      } else {
-        menuText.innerHTML = 'Menu'
-      };
+  closeButton.onclick = () => {
+    navMenu.classList.toggle('nav-main-open');
+    menuButtonWrap.classList.toggle('opened');
+    menuText.classList.toggle('menu-button-text-red');
+    if (menuText.innerHTML === 'Menu') {
+      menuText.innerHTML = 'Close'
+    } else {
+      menuText.innerHTML = 'Menu'
     };
   };
 })();
