c48665bb29ea0ca18d0a45c88c1bb7c0de0785b4
rhoover <robin.hoover@gmail.com>
2024-10-12 12:51:40 -0400

    Adapt Description Meta Tag

--- header.php
+++ header.php
@@ -68,7 +68,15 @@
 		media="tty" onload="this.media='screen' "
 	>
 
-	<meta name="description" content="My Blue Kitchen, Where Confidence Reluctance and Sass Come To A Boil">
+
+	<meta name="description" <?php
+		$post_slug = get_post_field('post_name', get_the_ID());
+		if ($post_slug == "france") {
+			echo ' content="My Blue Kitchen Presents The Flavors of France, A Pop Up Dinner Event"> ';
+		} else {
+			echo ' content="My Blue Kitchen, Where Confidence Reluctance and Sass Come To A Boil"> ';
+		};
+	?>
 	<meta name="apple-mobile-web-app-capable" content="yes">
 	<meta name="application-name" content="My Blue Kitchen">
 	<meta name="apple-mobile-web-app-title" content="My Blue Kitchen">
