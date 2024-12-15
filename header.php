110746b99d4a3a8bcdc4051f98a0d01e908857c4
rhoover <robin.hoover@gmail.com>
2024-12-15 12:30:37 -0500

    Accomodate New Header

--- header.php
+++ header.php
@@ -191,7 +191,7 @@
 		</section>
 		<section class="header-menu menu-toggle">
 				<svg class="icon icon-menu-toggle" aria-hidden="true" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 100 100"><g class="svg-menu-toggle"><path class="line line-1" d="M5 13h90v14H5z"/><path class="line line-2" d="M5 43h90v14H5z"/><path class="line line-3" d="M5 73h90v14H5z"/></g></svg>
-			<p class="menu-button-text" data-text-swap="Close">Menu</p>
+			<p class="menu-button-text" data-text-swap="Close">Site</p>
 		</section>
 	</header>
 
--- js/header-change.js
+++ js/header-change.js
@@ -13,6 +13,8 @@
   const menuIcon = document.querySelector('.icon');
   const menuText = document.querySelector('.menu-button-text');
 
+  const menuItems = document.querySelectorAll('.menu-item');
+
   window.addEventListener('scroll', () => {
     var scrollPos = window.scrollY;
     if (scrollPos >= mainElementPosition) {
@@ -22,6 +24,10 @@
       headerTitle.style.color = 'rgb(44, 112, 112)';
       menuIcon.style.fill = 'rgb(44, 112, 112)';
       menuText.style.color = 'rgb(44, 112, 112)';
+
+      menuItems.forEach(item => {
+        item.style.color =  'rgb(44, 112, 112)';
+      });
     } else {
       header.removeAttribute('style');
       headerArrow.removeAttribute('style');
@@ -29,6 +35,10 @@
       headerTitle.removeAttribute('style');
       menuIcon.removeAttribute('style');
       menuText.removeAttribute('style');
+
+      menuItems.forEach(item => {
+        item.removeAttribute('style');
+      });
     };
   });
 
