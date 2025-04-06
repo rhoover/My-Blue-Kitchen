40dd5cd1c035f68442ba0297bc20e284fdec5d32
rhoover <robin.hoover@gmail.com>
2025-04-06 13:07:17 -0400

    Trying To Make This Work

--- js/backbutton.js
+++ js/backbutton.js
@@ -2,18 +2,18 @@
     'use strict';
     var backButton = document.querySelector('.header-backbutton');
 
+    const landingPage = location.href
   // history.length is always 1 or 2 when landing on site for first time
   // it will be 2 if landing on site for the first time from URL in a new tab
   if (document.referrer == '' && history.length == 2) {
-    console.log('first if:', document.referrer, history.length);
+    console.log('first if:', document.referrer, history.length, landingPage);
     return;
   // it will be 1 if site was landed upon for the first time from a link somewhere else
   } else if (history.length == 1) {
-    console.log('second if:', document.referrer, history.length);
-    return;
-  // when both the above are false, show the back button
+    console.log('second if:', document.referrer, history.length, landingPage);
+    backButton.classList.add('header-backbutton-active');
   } else {
-    console.log('else:', document.referrer, history.length)
+    console.log('else:', document.referrer, history.length, landingPage);
     backButton.classList.add('header-backbutton-active');
   };
 
