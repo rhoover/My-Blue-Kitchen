6bcf9457f11e8ae4439b5a3c6d4e7facf1444f48
rhoover <robin.hoover@gmail.com>
2025-04-18 10:14:52 -0400

    Making It Work

--- js/backbutton.js
+++ js/backbutton.js
@@ -1,21 +1,19 @@
 	(function () {
     'use strict';
-    var backButton = document.querySelector('.header-backbutton');
+    let backButton = document.querySelector('.header-backbutton');
 
-    const landingPage = location.href
-  // history.length is always 1 or 2 when landing on site for first time
-  // it will be 2 if landing on site for the first time from URL in a new tab
-  if (document.referrer == '' && history.length == 2) {
-    console.log('first if:', document.referrer, history.length, landingPage);
-    return;
-  // it will be 1 if site was landed upon for the first time from a link somewhere else
-  } else if (history.length == 1) {
-    console.log('second if:', document.referrer, history.length, landingPage);
-    backButton.classList.add('header-backbutton-active');
-  } else {
-    console.log('else:', document.referrer, history.length, landingPage);
-    backButton.classList.add('header-backbutton-active');
-  };
+    // history.length is always 1 or 2 when landing on site for first time
+    // it will be 2 if landing on site for the first time from URL in a new tab
+    // if (document.referrer == '' && history.state == null) {
+    if (history.state == null && document.referrer == '') {
+      history.replaceState({ historyLength: history.length, href: location.href, page: 'Landing Page' }, "foo");
+      return;
+    // it will be 1 if site was landed upon for the first time from a link somewhere else
+    } else if (history.length == 1) {
+      return;
+    } else {
+      backButton.classList.add('header-backbutton-active');
+    };
 
     backButton.onclick = function () {
       history.back();
