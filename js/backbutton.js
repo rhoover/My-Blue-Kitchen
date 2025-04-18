e9ec063e290d45ae869086afc9e69b63ae984706
rhoover <robin.hoover@gmail.com>
2025-04-18 14:01:23 -0400

    Finally Working Right

--- js/backbutton.js
+++ js/backbutton.js
@@ -2,14 +2,18 @@
     'use strict';
     let backButton = document.querySelector('.header-backbutton');
 
+    // get rid of class on landing page
+    window.addEventListener('pageshow', function(event) {
+      if (event.persisted && history.state) {
+        backButton.classList.remove('header-backbutton-active');
+      };
+    });
+
     // history.length is always 1 or 2 when landing on site for first time
     // it will be 2 if landing on site for the first time from URL in a new tab
-    // if (document.referrer == '' && history.state == null) {
-    if (history.state == null && document.referrer == '') {
-      history.replaceState({ historyLength: history.length, href: location.href, page: 'Landing Page' }, "foo");
-      return;
     // it will be 1 if site was landed upon for the first time from a link somewhere else
-    } else if (history.length == 1) {
+    if ((history.state == null && document.referrer == '') || history.length == 1) {
+      history.replaceState({ historyLength: history.length, href: location.href, page: 'Landing Page' }, "foo");
       return;
     } else {
       backButton.classList.add('header-backbutton-active');
