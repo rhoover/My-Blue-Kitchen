5b1b1d8177e8367ef6ef1524a87b94458297baa6
rhoover <robin.hoover@gmail.com>
2025-05-21 16:41:56 -0400

    Improve WakeLock

--- js/wake-screen.js
+++ js/wake-screen.js
@@ -1,20 +1,69 @@
 (() => {
   'use strict';
-  let wakeButton = document.querySelector('.wake-screen-button');
 
-  if (navigator.wakeLock) {
+    let wakeButton = document.querySelector('.wake-screen-button');
+    let wakeButtonText = document.querySelector('.wake-screen-button-text');
 
-    wakeButton.style.display = "block";
+    // test support
+    let isSupported = false;
 
-    wakeButton.addEventListener('click', () => {
+    // change button and status if wakelock becomes aquired or is released
+    const changeUI = (status = 'acquired') => {
+      const acquired = status === 'acquired' ? true : false;
+      wakeButton.dataset.status = acquired ? 'on' : 'off';
+      wakeButtonText.textContent = `${acquired ? 'Screen Is Awake!' : 'Keep Screen Awake'}`;
+      // statusElem.textContent = `${acquired ? 'Screen Is Awake!' : 'Keep Screen Awake'}`;
+    };
+
+    if ('wakeLock in navigator') {
+      isSupported = true;
+      wakeButton.classList.add('wake-screen-button-active');
+    };
+
+    if (isSupported) {
+      // create a reference for the wake lock
+      let wakeLock = null;
+
+      // create an async function to request a wake lock
       const requestWakeLock = async () => {
-        let wakeState = null;
-        wakeState = await navigator.wakeLock.request("screen");
-        alert('Your screen will NOW STAY ON while you are cooking this recipe!');
-      };
-      requestWakeLock();
-    });
-  };
+        try {
+          wakeLock = await navigator.wakeLock.request('screen');
+
+          // change up our interface to reflect wake lock active
+          changeUI();
+
+          // listen for our release event
+          wakeLock.onrelease = function(ev) {
+            console.log(ev);
+          };
+
+          wakeLock.addEventListener('release', () => {
+            // if wake lock is released alter the button accordingly
+            changeUI('released');
+          });
+
+        } catch (err) {
+          // if wake lock request fails - usually system related, such as battery
+          wakeButton.dataset.status = 'off';
+          wakeButton.textContent = 'Turn Wake Lock ON';
+          statusElem.textContent = `${err.name}, ${err.message}`;
+        };
+      }; // requestWakeLock()
+
+      // if we click our button
+      wakeButton.addEventListener('click', () => {
+        // if wakelock is off request it
+        if (wakeButton.dataset.status === 'off') {
+          requestWakeLock()
+        } else { // if it's on release it
+          wakeLock.release()
+            .then(() => {
+              wakeLock = null;
+            });
+        };
+      });      
+    }; // end if isSupported
+
 
 
 
